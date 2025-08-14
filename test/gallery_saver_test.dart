29ba3758f49be3531866022092b9acedecea9d6a
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-08-14 16:07:33 -0600

    update test

--- test/gallery_saver_test.dart
+++ test/gallery_saver_test.dart
@@ -1,10 +1,11 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   const MethodChannel channel = MethodChannel('gallery_saver');
